f0dd368488a1dc42a89bf76201188903d520a9a4
Connor <connorkasarda@gmail.com>
2024-06-04 12:39:33 -0600

    combined update and render into run. renamed functions.

--- Portal/include/Root.h
+++ Portal/include/Root.h
@@ -6,10 +6,12 @@ namespace Portal
 	class Root
 	{
 	public:
-		void Initialize();
+		// Initializes Portal Engine systems in specific order
+		void Startup();
+		// Executes Portal Engine cycle until exit
+		void Run();
+		// Closes Portal Engine systems in reverese order
 		void Shutdown();
-		void Update();
-		void Render();
 	};
 }
 
